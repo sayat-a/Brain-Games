064e58c46ad91b48b4c72be295ae9c599ddbd248
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-06-08 00:37:04 +0500

    modify brain_games.py in scripts

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-from . import cli
+from brain_games import cli
 
 def main():
     print('Welcome to the Brain Games!')
+    cli.welcome_user()
     
 if __name__ == '__main__':
     main()
-    cli.welcome_user()
+    
