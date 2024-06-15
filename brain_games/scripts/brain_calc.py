a0483c0382510ca8ee205162adebd5a32feabd00
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-06-16 00:42:01 +0500

    modify brain_calc.py, brain_even.py, brain_gcd.py in scripts

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,38 +1,8 @@
-import prompt
-import random
-
-
-def is_even(n):
-    return n % 2 == 0
+from ..games import game_brain_calc
 
 
 def main():
-    print('Welcome to the Brain Games!')
-    user_name = prompt.string('May I have your name? ')
-    print(f'Hello, {user_name}!')
-    print('What is the result of the expression?')
-    count_right = 0
-    while count_right < 3:
-        number1 = random.randint(1, 100)
-        number2 = random.randint(1, 100)
-        operator = random.choice(['+', '-', '*'])
-        if operator == '+':
-            right_answer = number1 + number2
-        elif operator == '-':
-            right_answer = number1 - number2
-        elif operator == '*':
-            right_answer = number1 * number2
-        print(f'Question: {number1} {operator} {number2}')
-        answer = prompt.string('Your answer: ')
-        if int(answer) == right_answer:
-            count_right += 1
-            print('Correct!')
-        else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{right_answer}'.")
-            print(f"Let's try again, {user_name}!")
-            break
-    if count_right == 3:
-        print(f'Congratulations, {user_name}!')
+    game_brain_calc.play_brain_calc()
 
 
 if __name__ == '__main__':
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,34 +1,8 @@
-import prompt
-import random
-
-
-def is_even(n):
-    return n % 2 == 0
+from ..games import game_brain_even
 
 
 def main():
-    print('Welcome to the Brain Games!')
-    user_name = prompt.string('May I have your name? ')
-    print(f'Hello, {user_name}!')
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    count_right = 0
-    while count_right < 3:
-        number = random.randint(1, 100)
-        if is_even(number):
-            right_answer = 'yes'
-        elif not is_even(number):
-            right_answer = 'no'
-        print(f'Question: {number}')
-        answer = prompt.string('Your answer: ')
-        if answer == right_answer:
-            count_right += 1
-            print('Correct!')
-        else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{right_answer}'.")
-            print(f"Let's try again, {user_name}!")
-            break
-    if count_right == 3:
-        print(f'Congratulations, {user_name}!')
+    game_brain_even.play_brain_even()
 
 
 if __name__ == '__main__':
