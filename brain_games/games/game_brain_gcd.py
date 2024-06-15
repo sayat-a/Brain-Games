0acb2d7b89017f355ab5f57fdbc369f5c3686569
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-06-16 00:40:29 +0500

    add game_brain_calc.py, game_brain_even.py, game_brain_gcd.py to games

---
+++ brain_games/games/game_brain_gcd.py
@@ -0,0 +1,34 @@
+import prompt
+import random
+
+
+def find_gcd(number1, number2):
+    while number1 != number2:
+        if number1 > number2:
+            number1 = number1 - number2
+        else:
+            number2 = number2 - number1
+    return min(number1, number2)
+
+
+def play_brain_gcd():
+    print('Welcome to the Brain Games!')
+    user_name = prompt.string('May I have your name? ')
+    print(f'Hello, {user_name}!')
+    print('Find the greatest common divisor of given numbers.')
+    count_right = 0
+    while count_right < 3:
+        number1 = random.randint(10, 100)
+        number2 = random.randint(10, 100)
+        right_answer = find_gcd(number1, number2)
+        print(f'Question: {number1} {number2}')
+        answer = prompt.string('Your answer: ')
+        if int(answer) == right_answer:
+            count_right += 1
+            print('Correct!')
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{right_answer}'.")
+            print(f"Let's try again, {user_name}!")
+            break
+    if count_right == 3:
+        print(f'Congratulations, {user_name}!')
