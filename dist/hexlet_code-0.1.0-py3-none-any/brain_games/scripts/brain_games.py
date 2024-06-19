1404d9bfe6887338e104d3e27eb76e885915a436
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-06-19 18:52:28 +0500

    delete dist directory

--- dist/hexlet_code-0.1.0-py3-none-any/brain_games/scripts/brain_games.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from . import cli
-
-def main():
-    print('Welcome to the Brain Games!')
-    
-if __name__ == '__main__':
-    main()
-    cli.welcome_user()
--- dist/hexlet_code-0.1.0-py3-none-any/brain_games/scripts/cli.py
+++
@@ -1,6 +0,0 @@
-import prompt
-
-def welcome_user():
-    user_name = prompt.string('May I have your name? ')
-    print(f'Hello, {user_name}!')
-
